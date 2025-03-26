f3db824b9e2a8e08a5f17c33627a8e1c79af4302
rjenkins045 <150056979+rjenkins045@users.noreply.github.com>
2025-03-25 20:36:01 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,12 @@ variable "env" {
   type        = string
 }
 
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"  # optional default
+}
+
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
