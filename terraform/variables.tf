82fe4de9c52dc07048e848a3721652aefdc41b5c
rjenkins045 <150056979+rjenkins045@users.noreply.github.com>
2025-03-25 20:30:23 -0400

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "env" {
+  description = "Environment identifier (e.g., demo-env1)"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
