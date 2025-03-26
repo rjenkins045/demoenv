09bb757fef199e2e0d767fd7e8b661ddc2a34326
rjenkins045 <150056979+rjenkins045@users.noreply.github.com>
2025-03-25 20:22:20 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ module eks {
     cluster_name = var.cluster_name
     vpc_id = module.vpc.vpc_id
     subnet_ids = module.vpc.private_subnets
-    eks_mamanged_node_groups = {
+    eks_mananged_node_groups = {
         demo = {
             desired_size    = 1
             min_size        = 1
