658062c4fecc14adcc3fdb77ae412933dd5b9f11
rjenkins045 <150056979+rjenkins045@users.noreply.github.com>
2025-03-27 12:36:25 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,8 @@ variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
 }
+
+variable "assume_role_arn" {
+  description = "The ARN of the role to assume via STS"
+  type        = string
+}
